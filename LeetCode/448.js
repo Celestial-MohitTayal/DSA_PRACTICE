7f122f379d8e8120098ab19a27141834822f8c20
Celestial-MohitTayal <mohit.tayal@celestialsys.com>
2024-12-16 16:59:23 +0530

    commit

--- LeetCode/448.js
+++ LeetCode/448.js
@@ -1,31 +1,17 @@
 //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
 
 
-// var findDisappearedNumbers = function(nums) {
-//     let disappearedNum = [];
-//     for(let i = 1; i <= nums.length; i++){
-//         if(!nums.includes(i)){
-//             disappearedNum.push(i);
-//         }
-//     }
-
-//     return disappearedNum;
-// };
-
 function findDisappearedNumbers(nums) {
-    const n = nums.length;
+    let n = nums.length;
+    let map = new Array(n).fill(0);
 
-    for (let i = 0; i < n; i++) {
-        const num = Math.abs(nums[i]);
-        const index = num - 1;
-        if (nums[index] > 0) {
-            nums[index] = -nums[index];
-        }
+    for(let i = 0; i < n; i++){
+        map[nums[i] - 1] = 1;
     }
-    const result = [];
-    for (let i = 0; i < n; i++) {
-        if (nums[i] > 0) {
-            result.push(i + 1);  
+    const result = []
+    for(let i = 0; i< n; i++) {
+        if(map[i] == 0){
+            result.push(i+1);
         }
     }
 
